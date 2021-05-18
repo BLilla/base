45e24c04c9588e76c3589fadcba83463f832e194
BLilla <ballalilla0409@gmail.com>
2021-05-18 23:46:43 +0200

    Test cases added (task 2.2)

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,13 +1,7 @@
 package hu.bme.mit.train.sensor;
 
 import hu.bme.mit.train.interfaces.TrainController;
-import hu.bme.mit.train.interfaces.TrainUser;
-import hu.bme.mit.train.interfaces.TrainSensor;
 import hu.bme.mit.train.user.TrainUserImpl;
-import hu.bme.mit.train.controller.TrainControllerImpl;
-import hu.bme.mit.train.sensor.TrainSensorImpl;
-
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
@@ -15,20 +9,42 @@ import static org.mockito.Mockito.*;
 public class TrainSensorTest {
 	
 	TrainController controller;
-	TrainUser user;
-	TrainSensor sensor;
+	TrainUserImpl user;
+	TrainSensorImpl sensor;
 
     @Before
     public void before() {
-    	controller = new TrainControllerImpl();
-    	user = new TrainUserImpl(controller);
+    	controller = mock(TrainController.class);
+    	user = mock(TrainUserImpl.class);
     	sensor = new TrainSensorImpl(controller, user);
     }
 
     @Test
-    public void overrideSpeedLimitTest() {
-        sensor.overrideSpeedLimit(10);
-        Assert.assertEquals(10, sensor.getSpeedLimit());
- 
+    public void overAbsoluteMargin() {
+        when(controller.getReferenceSpeed()).thenReturn(600);
+        sensor.overrideSpeedLimit(600);
+        verify(user, times(1)).setAlarmState(true);
     }
+
+    @Test
+    public void underAbsoluteMargin() {
+        when(controller.getReferenceSpeed()).thenReturn(-50);
+        sensor.overrideSpeedLimit(-50);
+        verify(user, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void RelativeMargin() {
+        when(controller.getReferenceSpeed()).thenReturn(150);
+        sensor.overrideSpeedLimit(50);
+        verify(user, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void SetCorrectValueSpeedLimit() {
+        when(controller.getReferenceSpeed()).thenReturn(100);
+        sensor.overrideSpeedLimit(80);
+        verify(user, times(0)).setAlarmState(false);
+    }
+
 }
