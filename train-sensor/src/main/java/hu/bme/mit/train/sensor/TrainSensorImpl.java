dac6c3680b101f8e31e65602f149afe870110001
BLilla <ballalilla0409@gmail.com>
2021-05-18 22:16:30 +0200

    Implementing new feature (2.1 task)

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -22,6 +22,11 @@ public class TrainSensorImpl implements TrainSensor {
 
 	@Override
 	public void overrideSpeedLimit(int speedLimit) {
+		if(speedLimit < 0 || speedLimit > 500)
+			user.setAlarmState(true);
+		else if (controller.getReferenceSpeed() > speedLimit*2)
+			user.setAlarmState(true);
+
 		this.speedLimit = speedLimit;
 		controller.setSpeedLimit(speedLimit);
 	}
